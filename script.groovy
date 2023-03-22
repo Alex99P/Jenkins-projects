daca7d3b5f84176db64ba54bdc77186291711ee6
alexandru.patroi <alexandru.patroi@opswat.com>
2023-03-22 10:53:10 +0200

    add groovy script

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildJar(){
 def buildImage(){
     echo 'Building the docker image'
     withCredentials([usernamePassword(credentialsId:'docker-hub-repo', passwordVariable: 'PASS',usernameVariable:'USER' )]){
-        sh 'docker build -t alexpatroi/my-jenkins:jma-2.0 .'
+        sh 'docker build -t alexpatroi/my-jenkins:jma-3.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push alexpatroi/my-jenkins:jma-2.0'
 }
